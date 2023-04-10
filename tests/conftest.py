9f6d64a833a28a37b4957845951c63e5a7d6d7b7
Jocelyn <j.shao@hotmail.com>
2023-04-10 15:18:33 +0100

    changed import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
-from flask_app import create_app, config, data, user
+from flask_app.models import data, user
+from flask_app.__init__ import create_app
+from flask_app.config import config
 from selenium.webdriver.chrome.options import Options
 from dash.testing.application_runners import import_app
 
