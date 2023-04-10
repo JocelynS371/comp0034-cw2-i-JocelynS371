ebb35e13fe9beebad2fb1e443f23f97a2a4938bd
Jocelyn <j.shao@hotmail.com>
2023-04-10 14:42:33 +0100

    simple test to test if config is right

---
+++ tests/test_route.py
@@ -0,0 +1,12 @@
+import pytest
+import flask_app
+
+
+def test_index(test_client):
+    """
+    GIVEN a running Flask app
+    WHEN an HTTP GET request is made to '/'
+    THEN the status code should be 200"
+    """
+    response = client.get("/")
+    assert response.status_code == 200
