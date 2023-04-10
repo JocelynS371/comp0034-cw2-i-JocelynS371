27216827b73777e9083342d4e1fd7155b69a5f92
Jocelyn <j.shao@hotmail.com>
2023-04-10 17:57:29 +0100

    comment out hook for now

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,9 +28,9 @@ def new_user():
 
 
 
-def pytest_setup_options():
-    options = Options()
+#def pytest_setup_options():
+    #options = Options()
     # Uncomment the following if testing on GitHub actions, the browser needs to run in headless mode
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-    return options
+    #options.add_argument('--disable-gpu')
+    #options.add_argument('--headless')
+    #return options
