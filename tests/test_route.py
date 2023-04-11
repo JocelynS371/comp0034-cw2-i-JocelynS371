b3c8e00ed4dced8874bb5d3e327d11592da1fe7e
Jocelyn <j.shao@hotmail.com>
2023-04-11 11:59:10 +0100

    parametized test

--- tests/test_route.py
+++ tests/test_route.py
@@ -2,11 +2,16 @@ import pytest
 import flask_app
 
 
-def test_index(test_client):
+import pytest
+
+@pytest.mark.parametrize('route', ['/', '/login', '/register', '/data-entry', '/predict' ])
+def test_route_get(test_client, route):
     """
     GIVEN a running Flask app
-    WHEN an HTTP GET request is made to '/'
+    WHEN an HTTP GET request is made to functional routes
     THEN the status code should be 200"
     """
-    response = test_client.get("/")
+    response = test_client.get(route)
     assert response.status_code == 200
+
+
