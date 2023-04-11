720584ce10b197445b2f1bb79e571ce240832248
Jocelyn <j.shao@hotmail.com>
2023-04-11 14:53:05 +0100

    add another parametized test

--- tests/test_route.py
+++ tests/test_route.py
@@ -1,16 +1,43 @@
 import pytest
 import flask_app
+from flask_login import login_user
 
-
-import pytest
-
-@pytest.mark.parametrize('route', ['/', '/login', '/register', '/data-entry', '/predict' ])
-def test_route_get(test_client, route):
+@pytest.mark.parametrize('route, expected', [
+    ('/', 200),
+    ('/login', 200), 
+    ('/register', 200), 
+    ('/logout', 401),
+    ('/data-entry', 401),
+    ('/predict', 401)
+    ])
+def test_route_without_login(test_client, route, expected):
     """
     GIVEN a running Flask app
-    WHEN an HTTP GET request is made to functional routes
+    WHEN an HTTP GET request is made without login
+    WHEN the route does not require login
     THEN the status code should be 200"
+    WHEN the route require login
+    THEN the status code should be 401"
     """
     response = test_client.get(route)
-    assert response.status_code == 200
+    assert response.status_code == expected
+
+
+@pytest.mark.parametrize('route, expected', [
+    ('/', 200),
+    ('/login', 200), 
+    ('/register', 200),
+    ('/logout', 200), 
+    ('/data-entry', 200),
+    ('/predict', 200)
+    ])
+def test_route_with_login(test_client,user, route):
+    """
+    GIVEN a running Flask app
+    WHEN an HTTP GET request is made by login user
+    THEN all the status code should be 200"
+    """
+    login_user(user)
+    response = test_client.get(route)
+    assert response.status_code == expected
 
