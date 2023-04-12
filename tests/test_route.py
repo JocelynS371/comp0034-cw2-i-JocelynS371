4f9f5f9d0b96b39612132009dee352628b50e464
Jocelyn <j.shao@hotmail.com>
2023-04-12 16:22:33 +0100

    tried again with login testing

--- tests/test_route.py
+++ tests/test_route.py
@@ -23,23 +23,26 @@ def test_route_without_login(test_client, route, expected):
     assert response.status_code == expected
 
 
-#@pytest.mark.parametrize('route, expected', [
-#    ('/', 200),
-#    ('/login', 200), 
-#    ('/register', 200),
-#    ('/logout', 200), 
-#    ('/data-entry', 200),
-#    ('/predict', 200)
-#    ])
-#def test_route_with_login(app,test_client, route, expected):
-#    """
-#    GIVEN a running Flask app
-#    WHEN an HTTP GET request is made by login user
-#    THEN all the status code should be 200"
-#    """
-#    with app.test_request_context():
-#        test_client.post('/register', data=dict(username='dummy', password='dummy'))
-#        test_client.post('/login', data=dict(username='dummy', password='dummy'))
-#        response = test_client.get(route)
-#        assert response.status_code == expected
+import pytest
+
+@pytest.mark.parametrize('route, expected', [
+    ('/', 200),
+    ('/login', 200), 
+    ('/register', 200),
+    ('/logout', 200), 
+    ('/data-entry', 200),
+    ('/predict', 200)
+    ])
+def test_route_with_login(app, test_client, route, expected):
+    """
+    GIVEN a running Flask app
+    WHEN an HTTP GET request is made by login user
+    THEN all the status code should be 200"
+    """
+    with app.app_context()():
+        test_client.post('/register', data=dict(username='dummy', password='dummy'))
+        test_client.post('/login', data=dict(username='dummy', password='dummy'))
+        response = test_client.get(route)
+        assert response.status_code == expected
+
 
